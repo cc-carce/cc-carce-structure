72aad87abb95531c4a8b38d9b6f7f48de104f602
cyolo <33305168+cyolo@users.noreply.github.com>
2017-12-23 13:50:08 -0600

    Init

---
+++ src/main/java/com/carce/ccmx/CcCarceStructureApplication.java
@@ -0,0 +1,12 @@
+package com.carce.ccmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcCarceStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcCarceStructureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/carce/ccmx/CcCarceStructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carce.ccmx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CcCarceStructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
